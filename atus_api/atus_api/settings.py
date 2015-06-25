508a86e3d012710aff1019006f4387b1520a8563
Benjamin Phillips <benred42@gmail.com>
2015-06-25 14:26:27 -0400

    Changed project to use postgres

--- atus_api/atus_api/settings.py
+++ atus_api/atus_api/settings.py
@@ -75,9 +75,17 @@ WSGI_APPLICATION = 'atus_api.wsgi.application'
 # https://docs.djangoproject.com/en/1.8/ref/settings/#databases
 
 DATABASES = {
-    'default': {
+    'sqlite': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    },
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'atus_api',
+        'USER': 'atus_api',
+        'PASSWORD': '',
+        'HOST': '127.0.0.1',
+        'PORT': '5432'
     }
 }
 
