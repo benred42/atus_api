88ac38eaa2078df749713c3ba81543e33c85d909
Benjamin Phillips <benred42@gmail.com>
2015-06-25 23:31:36 -0400

    Got the number of respondents field working for the activity serializer

--- atus_api/api/views.py
+++ atus_api/api/views.py
@@ -1,10 +1,12 @@
 from api.models import Activity
 from api.serializers import ActivitySerializer
-from django.db.models import F, Sum
+from django.db.models import F, Sum, Count
 from rest_framework import viewsets
 
 
 class ActivityViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = Activity.objects.all().annotate(
-        weighted_average=(Sum(F('event__duration') * F('respondents__stat_wt')) / Sum('respondents__stat_wt')))
+        weighted_average=(
+        Sum(F('event__duration') * F('respondents__stat_wt')) / Sum('respondents__stat_wt'))).annotate(
+        num_respondents=Count('respondents'))
     serializer_class = ActivitySerializer
