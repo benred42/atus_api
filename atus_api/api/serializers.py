e9a589bf75c7b96853c1bcdf1602feeb4cbe5ae0
John Waldrep <johnwaldrep@gmail.com>
2015-06-26 08:08:31 -0400

    Add stub for  titles to ActivitySeializer; add code_list

--- atus_api/api/serializers.py
+++ atus_api/api/serializers.py
@@ -1,15 +1,23 @@
+from rest_framework.fields import SerializerMethodField
 from api.models import Activity, Respondent
 from rest_framework import serializers
-
+import api.code_list as atus
 
 class ActivitySerializer(serializers.HyperlinkedModelSerializer):
     code = serializers.PrimaryKeyRelatedField(source='tier_3', read_only=True)
     average_minutes = serializers.FloatField(read_only=True, source='weighted_average')
     total_respondents = serializers.IntegerField(read_only=True, source='num_respondents')
+    titles = SerializerMethodField()
+
+    def get_titles(self, obj):
+        # code = self.code
+        code = '130228' # FIXME: Set this equal to the 6 digit code of instance
+        titles =[atus.code_dict[i] for i in [code[0:2], code[0:4], code[0:6]]]
+        return titles
 
     class Meta:
         model = Activity
-        fields = ('code', 'average_minutes', 'total_respondents')
+        fields = ('code', 'average_minutes', 'total_respondents', 'titles')
 
 #######################################################################################################################
 
