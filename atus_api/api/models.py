d4363106aa0fbba0076f220d96fe1da89be869aa
Benjamin Phillips <benred42@gmail.com>
2015-06-25 20:42:33 -0400

    Added models for Activity, Respondent, and Event

--- atus_api/api/models.py
+++ atus_api/api/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
-# Create your models here.
+
+class Respondent(models.Model):
+    stat_wt = models.FloatField()
+    case_id = models.IntegerField()
+
+    def __str__(self):
+        return self.case_id
+
+
+class Activity(models.Model):
+    tier_1 = models.CharField(max_length=2)
+    tier_2 = models.CharField(max_length=4)
+    tier_3 = models.CharField(max_length=6)
+
+    def __str__(self):
+        return self.tier_3
+
+
+class Event(models.Model):
+    activity = models.ForeignKey(Activity)
+    respondent = models.ForeignKey(Respondent)
+    duration = models.FloatField()
