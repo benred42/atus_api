9fd745bc4a38583220b90f73388d6bf81c2d560c
John Waldrep <johnwaldrep@gmail.com>
2015-06-27 21:31:57 -0400

    Add hhmember serializer

--- atus_api/api/serializers.py
+++ atus_api/api/serializers.py
@@ -7,15 +7,18 @@ from rest_framework.reverse import reverse
 
 class ActivitySerializer(serializers.HyperlinkedModelSerializer):
     code = serializers.PrimaryKeyRelatedField(read_only=True)
-    average_minutes = serializers.FloatField(read_only=True, source='weighted_average')
-    total_respondents = serializers.IntegerField(read_only=True, source='num_respondents')
+    average_minutes = serializers.FloatField(read_only=True,
+                                             source='weighted_average')
+    total_respondents = serializers.IntegerField(read_only=True,
+                                                 source='num_respondents')
     titles = SerializerMethodField()
     filters = SerializerMethodField()
 
     def get_titles(self, obj):
         code = obj.code
         if len(obj.code) == 6:
-            titles = [atus.code_dict[i] for i in [code[0:2], code[0:4], code[0:6]]]
+            titles = [atus.code_dict[i] for i in
+                      [code[0:2], code[0:4], code[0:6]]]
         elif len(obj.code) == 4:
             titles = [atus.code_dict[i] for i in [code[0:2], code[0:4]]]
         else:
@@ -23,12 +26,15 @@ class ActivitySerializer(serializers.HyperlinkedModelSerializer):
         return titles
 
     def get_filters(self, obj):
-        filters = {key: value for key, value in self.context.get('request').GET.items() if key != 'page'}
+        filters = {key: value for key, value in
+                   self.context.get('request').GET.items() if key != 'page'}
         return filters
 
     class Meta:
         model = Activity
-        fields = ('url', 'code', 'average_minutes', 'total_respondents', 'titles', 'filters')
+        fields = (
+            'url', 'code', 'average_minutes', 'total_respondents', 'titles',
+            'filters')
 
 
 #######################################################################################################################
@@ -42,7 +48,7 @@ class EventSerializer(serializers.HyperlinkedModelSerializer):
 
     def get_activity_time(self, obj):
         code = sorted([activity.code for activity in obj.activity.all()])[-1]
-        return {code: (obj.duration/self.context.get('total_weight'))}
+        return {code: (obj.duration / self.context.get('total_weight'))}
 
 
 #######################################################################################################################
@@ -83,6 +89,32 @@ class RespondentSerializer(serializers.HyperlinkedModelSerializer):
 
     def get__links(self, obj):
         links = {
-            "activity_totals": reverse('event_list', kwargs=dict(respondent_id=obj.case_id),
-                                       request=self.context.get('request'))}
+            "activity_totals": reverse('event_list',
+                                       kwargs=dict(respondent_id=obj.case_id),
+                                       request=self.context.get('request')),
+            "household_members": reverse('hhmembers',
+                                         kwargs=dict(
+                                             respondent_id=obj.case_id),
+                                         request=self.context.get('request'))
+        }
         return links
+
+
+#######################################################################################################################
+
+class HouseholdMemberSerializer(serializers.HyperlinkedModelSerializer):
+    case_id = serializers.IntegerField(read_only=True)
+    hhmember_id = serializers.IntegerField(read_only=True)
+    age_edited = serializers.IntegerField(read_only=True)
+    relationship = serializers.IntegerField(read_only=True)
+    gender = serializers.IntegerField(read_only=True)
+    age_flag = serializers.IntegerField(read_only=True)
+    relationship_flag = serializers.IntegerField(read_only=True)
+    gender_flag = serializers.IntegerField(read_only=True)
+
+    class Meta:
+        model = Event
+        fields = (
+        'case_id', 'hhmember_id', 'age_edited', 'relationship', 'gender',
+        'age_flag', 'relationship_flag', 'gender_flag')
+
